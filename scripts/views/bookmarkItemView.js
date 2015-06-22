b81c598bd8903f04420ff0f4eaf031bd1ab9201e
John Hampton <john_j_hampton@yahoo.com>
2015-06-22 11:29:08 -0400

    Add event handler callback saveBookmark and log edited bookmark's title and URL to console

--- scripts/views/bookmarkItemView.js
+++ scripts/views/bookmarkItemView.js
@@ -4,7 +4,9 @@ export default Backbone.View.extend({
   className: 'bookmark-list-item',
 
   events: {
-    'click .bookmark-button-edit': 'editBookmark'
+    'click .bookmark-button-edit': 'editBookmark',
+    'submit .bookmark-form-edit':
+    'saveBookmark'
   },
 
   initialize: function() {
@@ -17,5 +19,14 @@ export default Backbone.View.extend({
 
   editBookmark: function() {
     console.log("edit clicked");
+  },
+
+  saveBookmark: function(event) {
+    event.preventDefault();
+    console.log("save button working", this.model.toJSON());
+    var title = this.$('.bookmark-title').val();
+    var url = this.$('.bookmark-url').val();
+    console.log(title, url);
+
   }
 });
