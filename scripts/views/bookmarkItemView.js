f5d046668251b271aa77c2eeb8a4107fc58671fe
John Hampton <john_j_hampton@yahoo.com>
2015-06-22 11:19:57 -0400

    Implement edit click handler

--- scripts/views/bookmarkItemView.js
+++ scripts/views/bookmarkItemView.js
@@ -3,11 +3,19 @@ export default Backbone.View.extend({
   tagName: 'li',
   className: 'bookmark-list-item',
 
+  events: {
+    'click .bookmark-button-edit': 'editBookmark'
+  },
+
   initialize: function() {
     this.render();
   },
 
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
+  },
+
+  editBookmark: function() {
+    console.log("edit clicked");
   }
 });
